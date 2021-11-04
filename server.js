36667a27ecea457c787389ae2d781ad3bc0b6e7e
urmade <turban2009@live.de>
2021-11-04 10:42:12 +0100

    Documented lab

--- server.js
+++ server.js
@@ -7,36 +7,25 @@ const apiHandler = require("./util/api");
 
 app.use("/static", express.static(path.join(__dirname, "static")));
 
-const { auth } = require('express-openid-connect');
-const { requiresAuth } = require('express-openid-connect');
-
-
 dotenv.config();
 
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.SECRET,
-    baseURL: 'http://localhost:3000',
-    clientID: process.env.CLIENT_ID,
-    issuerBaseURL: process.env.ISSUER_BASE_ID,
-};
+//TODO: Use Auth0 as a middleware for our project (Hint: Look at the quickstart tutorial)
 
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
-    if (req.oidc.isAuthenticated()) res.sendFile(path.join(__dirname, "pages", "order.html"));
-    else res.sendFile(path.join(__dirname, "pages", "index.html"));
+    //TODO Check if the user is authenticated
+    //if(<user is authenticated>) res.sendFile(path.join(__dirname, "pages", "order.html"));
+    //else
+    res.sendFile(path.join(__dirname, "pages", "index.html"));
 });
 
-app.get('/profile', requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.oidc.user));
+//TODO: Check if the user is authenticated before allowing to access to this endpoint
+app.get('/profile', (req, res) => {
+    //TODO send out the profile information
+    //res.send(<profileInfo>)
 });
 
+//Technical helpers, can be ignored
 app.use("/api", apiHandler);
-
-
 app.get("/callback", (req, res) => {
     res.redirect("/");
 })
